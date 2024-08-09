dea5734da35442a9b066b7145e318ba2c20dc9ce
Edgar Lopez <edgarlopez755@gmail.com>
2024-08-08 22:22:51 -0700

    Edit my quiz-box

--- js/app.js
+++ js/app.js
@@ -31,7 +31,7 @@ const quiz
 
 let firstQuestions = [
     {
-        numb: 1,
+        
         question: " Who won the 2023 Men's Royal Rumble match?",
         answer: "Cody Rhodes",
         options: ["The Undertaker", "Hulk Hogan", "John Cena"
@@ -39,7 +39,7 @@ let firstQuestions = [
         ]
     },
     {
-        numb: 2,
+        
         question: "Which wrestler holds the record for the most Royal Rumble eliminations?",
         answer: "Kane",
         options: ["Kurt Angle", "The Rock", "Stone Cold"
@@ -47,7 +47,7 @@ let firstQuestions = [
         ]
     },
     {
-        numb: 3,
+        
         question: "In what year did the first Women's Royal Rumble match take place?",
         answer: "2018",
         options: ["2004", "1995", "2012"
@@ -55,7 +55,7 @@ let firstQuestions = [
         ]
     },
     {
-        numb: 4,
+        
         question: "Who won the Women's Royal Rumble in 2024?",
         answer: "Bayley",
         options: ["Lita", "Trish Stratus", "Sasha Banks"
@@ -63,7 +63,7 @@ let firstQuestions = [
         ]
     },
     {
-        numb: 5,
+        
         question: "Which wrestler has the most Royal Rumble match victories?",
         answer: "Steve Austin",
         options: ["The Undertaker", "X-pac", "The 123 Kid"
@@ -71,7 +71,7 @@ let firstQuestions = [
         ]
     },
     {
-        numb: 6,
+        
         question: "Who was the first woman to win the Royal Rumble match?",
         answer: "Asuka",
         options: ["Victoria", "Miss Jackie", "Molly Holly"
@@ -79,7 +79,7 @@ let firstQuestions = [
         ]
     },
     {
-        numb: 7,
+        
         question: "What is the traditional number of participants in the Royal Rumble match?",
         answer: "30",
         options: ["25", "15", "10"
@@ -87,7 +87,7 @@ let firstQuestions = [
         ]
     },
     {
-        numb: 8,
+        
         question: "In what year did Stone Cold Steve Austin win the Royal Rumble?",
         answer: "1996",
         options: ["1990", "2023", "2002"
@@ -95,7 +95,7 @@ let firstQuestions = [
         ]
     },
     {
-        numb: 9,
+        
         question: "Which wrestler eliminated the most opponents in a single Royal Rumble match?",
         answer: "Kane",
         options: ["Brock Lesner", "The Rock", "John Cena"
@@ -103,7 +103,7 @@ let firstQuestions = [
         ]
     },
     {
-        numb: 10,
+        
         question: " Who won the 2020 Women's Royal Rumble match?",
         answer: "Charlotte Flair",
         options: ["Bayley", "Trish Stratus", "Lita"
@@ -117,7 +117,7 @@ let firstQuestions = [
 
 let secondQuestions = [
     {
-        numb: 1,
+        
         question: "Who was the first WWE Champion?",
         answer: "Lou Thesz",
         options: ["The Rock", "Hulk Hogan", "Ric Flair"
@@ -125,7 +125,7 @@ let secondQuestions = [
         ]
     },
     {
-        numb: 2,
+        
         question: " Which wrestler held the WWE Championship for the longest consecutive reign?",
         answer: "Hulk Hogan",
         options: ["Roman Reigns", "John Cena", "Stone Cold"
@@ -133,7 +133,7 @@ let secondQuestions = [
         ]
     },
     {
-        numb: 3,
+        
         question: "How many times has John Cena won the WWE Championship?",
         answer: "16 times",
         options: ["12 times", "7 times", "20 times"
@@ -141,7 +141,7 @@ let secondQuestions = [
         ]
     },
     {
-        numb: 4,
+        
         question: "Who was the youngest WWE Champion in history?",
         answer: "Brock Lesnar",
         options: ["Randy Orton", "The Rock", "Batista"
@@ -149,7 +149,7 @@ let secondQuestions = [
         ]
     },
     {
-        numb: 5,
+        
         question: "What is the name of The Rock's finishing move?",
         answer: "The Rock Bottom",
         options: ["The Smack Down", "The Eyebrow", "What's Cooking"
@@ -157,7 +157,7 @@ let secondQuestions = [
         ]
     },
     {
-        numb: 6,
+        
         question: "Which wrestler defeated Hulk Hogan to become the first WWF Champion?",
         answer: "The Iron Sheik",
         options: ["The Rock", "Kurt Angle", "Razor Ramon"
@@ -165,7 +165,7 @@ let secondQuestions = [
         ]
     },
     {
-        numb: 7,
+        
         question: "Who was the first WWE Women's Champion?",
         answer: "The Fabulous Moolah",
         options: ["Becky Linch", "Monica", "Mother Nature"
@@ -173,7 +173,7 @@ let secondQuestions = [
         ]
     },
     {
-        numb: 8,
+        
         question: "What is the name of the championship belt currently held by Cody Rhodes ?",
         answer: "WWE Championship",
         options: ["US Championship", "Womens Championship", "LV Belt"
@@ -181,7 +181,7 @@ let secondQuestions = [
         ]
     },
     {
-        numb: 9,
+        
         question: "Who was the first Monday Night Raw Triple Crown Champion?",
         answer: "Triple H",
         options: ["Kane", "Godfather", "Rick Rude"
@@ -189,7 +189,7 @@ let secondQuestions = [
         ]
     },
     {
-        numb: 10,
+        
         question: "Which wrestler held both the WWE Championship and the World Heavyweight Championship at the same time?",
         answer: "John Cena",
         options: ["The Rock", "Stone Cold", "The Undertaker"
